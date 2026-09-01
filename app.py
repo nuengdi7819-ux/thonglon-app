c2b48d5b8f6d5b54d2e795298b105ac11efd339e
nuengdi7819-ux <nuengdi7819@gmail.com>
2026-09-02 06:47:13 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -206,5 +206,12 @@ def update_payment(tx_id):
     db.session.commit()
     return redirect(request.referrer or url_for('index'))
 
-if __name__ == "__main__":
+@app.route('/delete_tx/<int:tx_id>')
+def delete_tx(tx_id):
+    tx = Transaction.query.get_or_404(tx_id)
+    db.session.delete(tx)
+    db.session.commit()
+    return redirect(request.referrer or url_for('index'))
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
